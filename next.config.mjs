8b177e96a884926da2308442374bf20a3e47fc21
um7a <um7a@users.noreply.github.com>
2024-09-01 14:10:32 -0700

    Adjust next.config.mjs more

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '/next-app-sample',
   basePath: '/next-app-sample',
+  assetPrefix: '/next-app-sample/',
 };
 
 export default nextConfig;

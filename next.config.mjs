f892ed89dcb515174118f4df1ab80df6bb9433b1
um7a <um7a@users.noreply.github.com>
2024-09-01 14:34:51 -0700

    Adjust page.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/next-app-sample',
-  assetPrefix: '/next-app-sample/',
-};
+const nextConfig = {};
 
 export default nextConfig;
